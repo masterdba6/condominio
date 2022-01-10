311b27d794107a29c7aa41ad1393cc14c16130f0
Robson Gomes Pedreira <masterdba6@gmail.com>
2022-01-10 17:45:02 -0300

    update

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import useApi from '../services/api'
 
 const DefaultLayout = () => {
+  const api = useApi()
+  const history = useHistory()
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      if (api.getToken()) {
+        const result = await api.validateToken()
+        if (result.error === '') {
+          setLoading(false)
+        } else {
+          alert(result.error)
+          history.push('/login')
+        }
+      } else {
+        history.push('/login')
+      }
+    }
+    checkLogin()
+  }, [])
+
   return (
     <div>
-      <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
-        <AppHeader />
-        <div className="body flex-grow-1 px-3">
-          <AppContent />
-        </div>
-        <AppFooter />
-      </div>
+      {!loading && (
+        <>
+          <AppSidebar />
+          <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+            <AppHeader />
+            <div className="body flex-grow-1 px-3">
+              <AppContent />
+            </div>
+            <AppFooter />
+          </div>
+        </>
+      )}
     </div>
   )
 }
